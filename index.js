dbc98dd6d091dcfca021c2d54d86fd800567dd6f
TheTrabin <beyondtears@gmail.com>
2020-07-07 19:37:45 -0700

    Added GET function.

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const express = require("express");
 //punch variable as function: Set up server.
 const server = express();
 
+// GET /  server.METHOD(path, callback(request(req), response(res)))
+server.get('/', (request, response) => {
+    response.json({hello:'world'});
+})
+
+
+//signify port
+const PORT = 5000;
+
 //call the server into listening: Port, callback
-server.listen(5000, () => {
+server.listen(PORT, () => {
     console.log('listening on localhost:', 5000);
 })
 //run by using node index.js in console
